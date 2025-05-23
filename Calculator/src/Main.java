0e0dd0c2cf3773d3c20ac37b49dbf8d13c88065d
popovnikitaserg <144046985+popovnikitaserg@users.noreply.github.com>
2025-05-23 19:18:31 +0300

    Edit method isNum to isNumCorrect and make it accept numbers from 1 to 10

--- Calculator/src/Main.java
+++ Calculator/src/Main.java
@@ -14,7 +14,7 @@ public class Main {
     public static String calc(String input) throws Exception{
         String[] calculus = input.split(" ");
         if (calculus.length == 3) {
-            if (isNum(calculus[0]) && isNum(calculus[2])) {
+            if (isNumCorrect(calculus[0]) && isNumCorrect(calculus[2])) {
                 if (calculus[1].equals("+")) {
                     return Integer.toString(Integer.parseInt(calculus[0]) + Integer.parseInt(calculus[2]));
                 } else if (calculus[1].equals("-")) {
@@ -29,10 +29,10 @@ public class Main {
         throw new Exception();
     }
 
-    public static boolean isNum(String str) {
+    public static boolean isNumCorrect(String str) {
         try {
-            Integer.parseInt(str);
-            return true;
+            int num = Integer.parseInt(str);
+            return num > 0 && num < 11;
         } catch (NumberFormatException e) {
             return false;
         }
